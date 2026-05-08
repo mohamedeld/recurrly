6f6565bbfea3ff7d87d5c1d7cb219ca3686acf5c
mohamedelrfaay <mohamed.elrfaay@lebara.sa>
2026-05-08 11:27:33 +0300

    add native wind successfully

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
